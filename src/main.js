9791b6520cd3f9daf9d8f05582c05d86b58b8867
I-the-Creator <andrey.burluckiy@gmail.com>
2022-03-28 21:56:21 +0200

    styles imports added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Styles
+import './assets/styles/style.scss'
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
