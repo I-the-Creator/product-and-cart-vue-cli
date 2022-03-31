9acb4f89c7d78584f85ddb5891802e8c0ba00781
I-the-Creator <andrey.burluckiy@gmail.com>
2022-03-31 17:05:32 +0200

    Home component created; properties (props, components etc) added

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+
+    <div class="splash-container">
+      <div class="splash">
+        <h1>Splendid Food</h1>
+      </div>
+    </div>
+
+    <main class="wrapper">
+      <h2>Recommended</h2>
+
+      <div class="recommended">
+        <!-- :key for iterable items -->
+        <ProductCard
+          v-for="(product, index) in inventory.slice(0, 3)"
+          :key="product.id"
+          class="card"
+          :index="index"
+          :product="product"
+          :addToCart="addToCart"
+        />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue'
+
+export default {
+  name: 'HomeView',
+  props: ['inventory', 'addToCart'], // get props from App.js component
+  components: {
+    ProductCard
+  }
+}
+</script>
